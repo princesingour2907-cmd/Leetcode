b40818aeebe603f397bc54f62858b012e46bb361
Prince Singour <princesingour2907@gmail.com>
2026-07-07 10:36:38 +0530

    Time: 1573 ms (10.52%), Space: 46.5 MB (38.33%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(isPalindrome(s,i,j)){
+                    if(j-i+1>ans.length()){
+                         ans=s.substring(i,j+1);
+                    }
+
+                }
+            }
+        } return ans;
+    }
+    public boolean isPalindrome(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
