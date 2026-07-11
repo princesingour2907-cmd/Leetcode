f26d79e593a9a517e995016763f01cca6eaa3762
Prince Singour <princesingour2907@gmail.com>
2026-07-11 21:52:29 +0530

    Time: 0 ms (100%), Space: 43 MB (18.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
      int dp[]= new int[nums.length];
-     Arrays.fill(dp,-1);
-      return chorr(0,nums,dp); 
+      return chorr(nums,dp); 
        
     }
-    public int chorr(int i,int[] nums,int [] dp){
-        if(i>=nums.length){
-            return 0 ;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int pick=nums[i]+chorr(i+2,nums,dp);
-        int skip=chorr(i+1,nums,dp);
-        return dp[i]=Math.max(pick,skip);
+    public int chorr(int[] nums,int [] dp){
+        if(nums.length==0) return 0;
+       if(nums.length==1) return nums[0];
+       dp[0]=nums[0];
+       dp[1]=Math.max(nums[0],nums[1]);
+       
+       for(int i=2;i<nums.length;i++){
+         dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+       }
+       return dp[dp.length-1];
     }
 }
